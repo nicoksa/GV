873c91282b11d600ce73d6727bfb1c2784bc4e25
Nicolás Casarino <nicolascasarino@gmail.com>
2025-07-02 21:40:03 -0300

    Empece con la logica de la vista Property

--- Pages/Property.cshtml.cs
+++ Pages/Property.cshtml.cs
@@ -1,12 +1,51 @@
+using GV.Data;
+using GV.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace GV.Pages
 {
     public class PropertyModel : PageModel
     {
-        public void OnGet()
+        private readonly AppDbContext _context;
+
+        public Propiedad? Propiedad { get; set; }
+        public PropiedadUrbana? PropiedadUrbana { get; set; }
+        public PropiedadCampo? PropiedadCampo { get; set; }
+        public bool IsUrban { get; set; }
+
+        public PropertyModel(AppDbContext context)
         {
+            _context = context;
+        }
+
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            Propiedad = await _context.Propiedades
+                .Include(p => p.Imagenes)
+                .Include(p => p.Videos)
+                .FirstOrDefaultAsync(p => p.Id == id);
+
+            if (Propiedad == null)
+            {
+                return NotFound();
+            }
+
+            if (Propiedad.Division == "Urbano")
+            {
+                PropiedadUrbana = await _context.PropiedadesUrbanas
+                    .FirstOrDefaultAsync(pu => pu.Id == id);
+                IsUrban = true;
+            }
+            else if (Propiedad.Division == "Campo")
+            {
+                PropiedadCampo = await _context.PropiedadesCampo
+                    .FirstOrDefaultAsync(pc => pc.Id == id);
+                IsUrban = false;
+            }
+
+            return Page();
         }
     }
 }
