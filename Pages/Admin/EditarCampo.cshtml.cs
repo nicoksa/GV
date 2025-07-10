0f4b5f1356bf72451f8da3d6ed2af44007c07fd7
unknown <nicolascasarino@gmail.com>
2025-07-10 18:08:20 -0300

    Sigo con admin

---
+++ Pages/Admin/EditarCampo.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GV.Pages.Admin
+{
+    public class EditarCampoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Admin/EditarPropiedad.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GV.Pages.Admin
+{
+    public class EditarPropiedadModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
