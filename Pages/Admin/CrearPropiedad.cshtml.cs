34df7acb6a4c3ea90926cd054736cc8aafcb0c0e
Nicolás Casarino <nicolascasarino@gmail.com>
2025-07-10 22:38:46 -0300

    Agregue Crear propiedades y campos

---
+++ Pages/Admin/CrearPropiedad.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GV.Pages.Admin
+{
+    public class CrearPropiedadModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
